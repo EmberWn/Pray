9606fb31faa454361dcc7da80a76d9431172924f
EmberWn <2289710829@qq.com>
2019-12-25 15:57:21 +0800

    finish throw coins

---
+++ Config.py
@@ -0,0 +1,28 @@
+# -*- coding:utf8 -*-
+
+
+class ParamConfig:
+    THROW_COUNT = 6
+    COIN_NUM = 3
+    RESULTS_COUNT = 4
+    ONE_TRIGRAM_THROW_COUNT = 3
+    YINANDYANG = 2
+    YIN = 0
+    YANG = 1
+    RESULTS = {
+            0: 'old yin',
+            1: 'young yin',
+            2: 'young yang',
+            3: 'old yang',
+    }
+    EightTrigrams = {
+                0: '坤',
+                7: '乾',
+                1: '震',
+                4: '艮',
+                5: '离',
+                2: '坎',
+                3: '兑',
+                6: '巽',
+    }
+
--- main.py
+++ main.py
@@ -1,34 +1,55 @@
 # -*- coding:utf8 -*-
+import random
+from Config import ParamConfig
 
 
 class Predict(object):
-
-    THROW_COUNT = 6
-    COIN_NUM = 3
-    results = {
-            0: 'old sun',
-            1: 'young sun',
-            2: 'young moon',
-            3: 'old moon'
-    }
 
     def __init__(self):
         self.result = []
 
     def throw_once(self):
-        pass
+        rand_num = random.randint(0, ParamConfig.RESULTS_COUNT - 1)
+        if rand_num < ParamConfig.YINANDYANG:
+            num = ParamConfig.YIN
+        else:
+            num = ParamConfig.YANG
+        self.result.append(num)
 
     def start(self):
-        for x in xrange(0, self.THROW_COUNT):
+        for x in xrange(0, ParamConfig.THROW_COUNT):
             self.throw_once()
         self.parse_result()
 
     def parse_result(self):
-        pass
+        self.result.reverse()
+        nums = []
+        start = 0
+        length = len(self.result)
+        print self.result
+        while True:
+            end = start + ParamConfig.ONE_TRIGRAM_THROW_COUNT
+            trigram = self.result[start: end]
+            nums.append(self.calculate(trigram))
+            start = end
+            if start == length:
+                break
+        print nums
+        self.output_result(nums)
+
+    def calculate(self, trigram):
+        num = 0
+        for i in trigram:
+            num = i + (num << 1)
+        return num
+
+    def output_result(self, nums):
+        print ParamConfig.EightTrigrams[nums[0]], ParamConfig.EightTrigrams[nums[1]]
 
 
 def main():
-    pass
+    pre = Predict()
+    pre.start()
 
 
 if __name__ == '__main__':
